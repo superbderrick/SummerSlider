5dd49df450b16fd8e6be8d34e2a831fefaeb28d4
derrick <kang.derrick@gmail.com>
2017-09-10 18:43:10 +0900

    added vast example.

---
+++ Example/vastExample/vastExample/vastExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vastExample
+//
+//  Created by Kang Jinyeoung on 10/09/2017.
+//  Copyright © 2017 SuperbDerrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
