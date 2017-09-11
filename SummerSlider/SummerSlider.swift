df71ac2097bdad42f9b4ee7b26c4e4d6e09958d7
derrick <kang.derrick@gmail.com>
2017-09-11 21:11:34 +0900

    changed restriction type

--- SummerSlider/SummerSlider.swift
+++ SummerSlider/SummerSlider.swift
@@ -9,74 +9,74 @@
 import UIKit
 
 public class SummerSlider: UISlider {
-
-	var markColor : UIColor!
-	var markWidth : CGFloat!
-	var markPositions : Array<Float>!
-	var selectedBarColor : UIColor!
-	var unselectedBarColor : UIColor!
-	
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-		
-		self.setupDefaultValues()
-	}
-	
-	required init?(coder aDecoder: NSCoder) {
-		super.init(coder: aDecoder)
-		fatalError("init(coder:) has not been implemented")
-   // self.setupDefaultValues()
-	}
- 
-	override func draw(_ rect: CGRect) {
-		super.draw(rect)
+  
+  var markColor : UIColor!
+  var markWidth : CGFloat!
+  var markPositions : Array<Float>!
+  var selectedBarColor : UIColor!
+  var unselectedBarColor : UIColor!
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    self.setupDefaultValues()
+  }
+  
+  required public init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    fatalError("init(coder:) has not been implemented")
+    // self.setupDefaultValues()
+  }
+  
+  override public func draw(_ rect: CGRect) {
+    super.draw(rect)
     
     // Basic Setup to make a Inner Rect.
-		let innerRect = rect.insetBy(dx: Constants.INNER_RECT.X, dy: Constants.INNER_RECT.Y)
-		UIGraphicsBeginImageContextWithOptions(innerRect.size ,false , 0)
-		let context = UIGraphicsGetCurrentContext()!
+    let innerRect = rect.insetBy(dx: Constants.INNER_RECT.X, dy: Constants.INNER_RECT.Y)
+    UIGraphicsBeginImageContextWithOptions(innerRect.size ,false , 0)
+    let context = UIGraphicsGetCurrentContext()!
     
     // Draw Selection Rect.
     SummerSlider.drawRect(context,  innerRect , self.selectedBarColor.cgColor)
-		
+    
     // Make Selection Image.
-		let selectedSide = UIGraphicsGetImageFromCurrentImageContext()
-		selectedSide!.resizableImage(withCapInsets: UIEdgeInsets.zero)
-		
+    let selectedSide = UIGraphicsGetImageFromCurrentImageContext()
+    selectedSide!.resizableImage(withCapInsets: UIEdgeInsets.zero)
+    
     // Draw UNSelection Rect.
     SummerSlider.drawRect(context,  innerRect , self.unselectedBarColor.cgColor)
-		
-    // Make UNSelection Image.
-		let unSelectedSide = UIGraphicsGetImageFromCurrentImageContext()
-		unSelectedSide!.resizableImage(withCapInsets: UIEdgeInsets.zero)
-		
-    // Draw Selection Image.
-		selectedSide!.draw(at: CGPoint(x:0,y:0))
-		
-    // Draw Selection Marks.
-		SummerSlider.drawMarks(context, innerRect, self.markColor.cgColor, self.markPositions, self.markWidth)
-
-
-		let selectedStripSide = UIGraphicsGetImageFromCurrentImageContext()!.resizableImage(withCapInsets: UIEdgeInsets.zero)
-		
-    // Draw UNSelection Image.
-		unSelectedSide!.draw(at: CGPoint(x:0,y:0))
     
-
+    // Make UNSelection Image.
+    let unSelectedSide = UIGraphicsGetImageFromCurrentImageContext()
+    unSelectedSide!.resizableImage(withCapInsets: UIEdgeInsets.zero)
+    
+    // Draw Selection Image.
+    selectedSide!.draw(at: CGPoint(x:0,y:0))
+    
+    // Draw Selection Marks.
     SummerSlider.drawMarks(context, innerRect, self.markColor.cgColor, self.markPositions, self.markWidth)
-
-		
-		let unselectedStripSide = UIGraphicsGetImageFromCurrentImageContext()!.resizableImage(withCapInsets: UIEdgeInsets.zero)
-		
-		UIGraphicsEndImageContext()
-		
-		setupTrackRange(selectedStripSide, unselectedStripSide)
-	}
+    
+    
+    let selectedStripSide = UIGraphicsGetImageFromCurrentImageContext()!.resizableImage(withCapInsets: UIEdgeInsets.zero)
+    
+    // Draw UNSelection Image.
+    unSelectedSide!.draw(at: CGPoint(x:0,y:0))
+    
+    
+    SummerSlider.drawMarks(context, innerRect, self.markColor.cgColor, self.markPositions, self.markWidth)
+    
+    
+    let unselectedStripSide = UIGraphicsGetImageFromCurrentImageContext()!.resizableImage(withCapInsets: UIEdgeInsets.zero)
+    
+    UIGraphicsEndImageContext()
+    
+    setupTrackRange(selectedStripSide, unselectedStripSide)
+  }
   
   
   private func setupTrackRange(_ selectedStripSide:UIImage , _ unselectedStripSide:UIImage) {
     self.setMinimumTrackImage(selectedStripSide, for: UIControlState.normal)
-		self.setMaximumTrackImage(unselectedStripSide, for: UIControlState.normal)
+    self.setMaximumTrackImage(unselectedStripSide, for: UIControlState.normal)
   }
   
   private func setupDefaultValues(){
@@ -109,5 +109,5 @@ public class SummerSlider: UISlider {
     context.setStrokeColor(rectColr)
     context.strokePath()
   }
-
+  
 }
