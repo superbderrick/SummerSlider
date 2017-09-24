3ff331debd4dff5ec82b4dc75d6425394dfac3d8
derrick <kang.derrick@gmail.com>
2017-09-24 13:35:06 +0900

    working on basic sample for Horizontal case.

--- Example/basicExample/SummerSlider/HorizontalViewController.swift
+++ Example/basicExample/SummerSlider/HorizontalViewController.swift
@@ -7,29 +7,42 @@
 //
 
 import UIKit
-
+import SummerSlider
 class HorizontalViewController: UIViewController {
+  
+    var testSlider1:SummerSlider!
+    var testSlider2:SummerSlider!
+    var testSlider3:SummerSlider!
+  
+    let testRect1 = CGRect(x:30 ,y:100 , width:300 , height:30)
+    let testRect2 = CGRect(x:30 ,y:150 , width:300 , height:30)
+    let testRect3 = CGRect(x:30 ,y:200 , width:300 , height:30)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      testSlider1 = SummerSlider(frame: testRect1)
+      
+      
+      testSlider2 = SummerSlider(frame: testRect2)
+      
+      testSlider3 = SummerSlider(frame: testRect3)
+      
+      
+      
+      self.view.addSubview(testSlider1)
+      self.view.addSubview(testSlider2)
+      self.view.addSubview(testSlider3)
 
-        // Do any additional setup after loading the view.
+     
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+     
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
