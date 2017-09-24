e8b0efb71bf0a9ec8019965d6f2b967d8dd299ab
derrick <kang.derrick@gmail.com>
2017-09-24 17:49:53 +0900

    working on basic project structure.

---
+++ SummerSlider/SummerSlider.h
@@ -0,0 +1,19 @@
+//
+//  SummerSlider.h
+//  SummerSlider
+//
+//  Created by Kang Jinyeoung on 24/09/2017.
+//  Copyright © 2017 SuperbDerrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SummerSlider.
+FOUNDATION_EXPORT double SummerSliderVersionNumber;
+
+//! Project version string for SummerSlider.
+FOUNDATION_EXPORT const unsigned char SummerSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SummerSlider/PublicHeader.h>
+
+
