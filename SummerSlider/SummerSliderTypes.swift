74dd1e4fe1745af3aebcbb1d7eb554e2846ae43b
derrick <kang.derrick@gmail.com>
2017-09-25 21:11:02 +0900

    added type class

---
+++ SummerSlider/SummerSliderTypes.swift
@@ -0,0 +1,21 @@
+//
+//  SummerSliderTypes.swift
+//  SummerSlider
+//
+//  Created by derrick on 25/09/2017.
+//  Copyright © 2017 SuperbDerrick. All rights reserved.
+//
+
+import Foundation
+
+
+public enum DrawingMode {
+	case BothSides
+	case UnselectedOnly
+	case SelectedOnly
+}
+
+public enum Orientation {
+	case Horizontal
+	case Vertical
+}
